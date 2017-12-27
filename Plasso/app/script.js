39cf98f5e9b6aa8848482402c740d61c306169f6
Ibrahim <ibrahimbutt1993@gmail.com>
2017-12-27 20:18:38 +0500

    WIP Slides

--- Plasso/app/script.js
+++ Plasso/app/script.js
@@ -12,3 +12,16 @@ var slides = document.getElementById('slides');
   billingBottom.outerHTML = '<div class="slides__background__bottom--billing is-active" id="billingBottom"></div>';
   billingTop.outerHTML = ' <div class="slides__background__top--billing is-active" id="billingTop"></div>';
 });
+
+// // First we detect the click event
+// document.getElementById('the-box').addEventListener('click', function () {
+//   // Using an if statement to check the class
+//   if (this.classList.contains('bad')) {
+//     // The box that we clicked has a class of bad so let's remove it and add the good class
+//    this.classList.remove('bad');
+//    this.classList.add('good');
+//   } else {
+//     // The user obviously can't follow instructions so let's alert them of what is supposed to happen next
+//     alert("You can proceed!");
+//   }
+// });
