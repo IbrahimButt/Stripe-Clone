50b3c112ce4cb194959d915318090ed915aed312
Ibrahim Butt <ibrahimbutt1993@gmail.com>
2018-02-06 23:07:01 +0000

    EOD

--- Plasso/app/script.js
+++ Plasso/app/script.js
@@ -1,9 +1,20 @@
 var hamburger = document.getElementById('hamburger');
+var header = document.getElementById('header')
 
 hamburger.addEventListener('click', function() {
-  if (this.classList.contains('active')) {
+
+  if (this.classList.contains('active'))
     this.classList.remove('active');
-  } else {
+  else
     this.classList.add('active');
-  }
+
+})
+
+window.addEventListener('scroll', function() {
+
+  if (scrollY < 30)
+    header.classList.remove('shadowOnScroll');
+  else
+    header.classList.add('shadowOnScroll');
+    
 })
